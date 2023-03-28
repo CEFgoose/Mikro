aa47c44e29fcaefdefb5daf3e0f1ff63cde7f875
CEFgoose <commonwealthgoose@gmail.com>
2023-03-28 10:59:00 -0600

    changed variables for deployment

--- frontend/Mikro/src/components/constants.js
+++ frontend/Mikro/src/components/constants.js
@@ -1,6 +1,6 @@
 //PROD
-// export const API_URL = "https://mikro.kaart.com/api/";
-// export const SSO_URL = "https://my.kaart.com/api/";
+export const API_URL = "https://mikro.kaart.com/api/";
+export const SSO_URL = "https://my.kaart.com/api/";
 //DEV
-export const API_URL = "http://dev.localhost:5004/api/";
-export const SSO_URL = "http://dev.localhost:5001/api/";
+// export const API_URL = "http://dev.localhost:5004/api/";
+// export const SSO_URL = "http://dev.localhost:5001/api/";
