c6973a45551dd63268a249d7e8214c4363b595d5
CEFgoose <commonwealthgoose@gmail.com>
2023-03-28 14:58:14 -0600

    deployment variable changes

--- frontend/Mikro/src/components/Login/index.js
+++ frontend/Mikro/src/components/Login/index.js
@@ -47,7 +47,7 @@ export const Login = () => {
     let city;
     fetch(url, {
       method: "post",
-      mode: "cors",
+      // mode: "cors",
       credentials: "include",
       headers: {
         "X-CSRF-TOKEN": `${Cookie.get("csrf_access_token")}`,
@@ -85,10 +85,10 @@ export const Login = () => {
             e.preventDefault();
             fetch(SSO_URL.concat("auth/login"), {
               method: "POST",
-              mode: "cors",
+              // mode: "cors",
               credentials: "include",
               headers: {
-                "Access-Control-Allow-Origin": "*",
+                // "Access-Control-Allow-Origin": "*",
                 "Content-Type": "application/json",
               },
               body: JSON.stringify({
